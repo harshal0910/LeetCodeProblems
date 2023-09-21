7fb69719c604050c7d2004f57b49941d188cdd63
harshal0910 <130588038+harshal0910@users.noreply.github.com>
2023-09-21 09:48:29 +0530

    Update ReverseWordsInString.java

--- ReverseWordsInString.java
+++ ReverseWordsInString.java
@@ -29,7 +29,7 @@ class Solution {
             String []str=s.trim().split("\\s+");
             StringBuilder sb=new StringBuilder();
             for(int i=str.length-1;i>=0;i--){
-                sb.append(splitted[i]);
+                sb.append(str[i]);
                 if(i!=0){
                     sb.append(" ");
                 }
