549385fc12eb1782b8d968cad655a3f141b99a40
harshal0910 <130588038+harshal0910@users.noreply.github.com>
2023-09-26 18:49:26 +0530

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -36,3 +36,15 @@ class Solution {
         return 0;
     }
 }
+/*
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++){
+            ans=ans^nums[i];
+        }
+
+        return ans;
+    }
+}
+*/
