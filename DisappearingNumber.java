f4c2088634ca88db1429f3d2d3562c33478e32df
harshal0910 <130588038+harshal0910@users.noreply.github.com>
2024-02-04 22:07:33 +0530

    Update DisappearingNumber.java

--- DisappearingNumber.java
+++ DisappearingNumber.java
@@ -43,3 +43,22 @@ class Solution {
         return list;
     }
 }
+
+
+/*
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<=nums.length;i++){
+            if(!set.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
+*/
